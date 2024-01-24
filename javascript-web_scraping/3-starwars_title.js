53c7199f95ebd07ac2afe503787fa482701db9f4
Adib-96 <adibbh1996@gmail.com>
2024-01-24 08:05:46 +0100

    Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const movieId = process.argv[2];
+const startWarsUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(startWarsUrl, (error, response, body) => {
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
