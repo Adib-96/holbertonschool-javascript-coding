80c2bf3b2db0ca29c8f6108f12b1f737dda434df
Adib-96 <adibbh1996@gmail.com>
2024-03-08 21:55:12 +0100

    small http server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
-const http = require('http')
-const app= http.createServer((req,res) => {
-    console.log(res.end("Hello Holberton School!"));
-})
-app.listen(1245)
+const http = require("http");
+const app = http.createServer((req, res) => {
+  console.log(res.end("Hello Holberton School!"));
+});
+app.listen(1245);
 module.exports = app;
