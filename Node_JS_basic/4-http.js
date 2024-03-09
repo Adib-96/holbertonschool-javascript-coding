b1e4cdedaaf217a0320e3c33d367e1ea0caef757
Adib-96 <adibbh1996@gmail.com>
2024-03-09 14:26:05 +0100

    create a small http server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
-const http = require("http");
+const http = require('http');
 
 const app = http.createServer((req, res) => {
-  console.log(res.end("Hello Holberton School!"));
+  console.log(res.end('Hello Holberton School!'));
 });
 app.listen(1245);
 module.exports = app;
