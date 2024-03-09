698e246c30bac21e1bfc8511ae8782adb11d94ee
Adib-96 <adibbh1996@gmail.com>
2024-03-09 14:47:16 +0100

    async file sys reader

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,30 @@
 const fs = require('node:fs/promises');
 
-
 async function countStudents(path) {
-    let data;
-    try {
-        data = await fs.readFile(path, { encoding: 'utf-8' });
+  let data;
+  try {
+    data = await fs.readFile(path, { encoding: 'utf-8' });
+  } catch (err) {
+    console.log('Cannot load the database');
+  }
+  const content = data.split('\n');
+  const tableOfUsers = content.map((row) => row.split('\n')).slice(1, content.length);
+  const STUDENT_NUMBER = content.slice(1, content.length).length;
+  const magic = [];
+  for (let i = 0; i < STUDENT_NUMBER; i += 1) {
+    magic.push(tableOfUsers[i][0].split(','));
+  }
+  const csStudent = magic.filter((user) => user[3] === 'CS');
+  const sweStudent = magic.filter((user) => user[3] === 'SWE');
 
-    } catch (err) {
-        console.log('Cannot load the database');
-    }
-    const content = data.split('\n');
-    const tableOfUsers = content.map(row => row.split('\n')).slice(1, content.length);
-    const STUDENT_NUMBER = content.slice(1, content.length).length;
-    const magic = [];
-    for (let i = 0; i < STUDENT_NUMBER; i++) {
-        magic.push(tableOfUsers[i][0].split(','));
-    }
-    const csStudent = magic.filter(user => user[3] === 'CS')
-    const sweStudent = magic.filter(user => user[3] === 'SWE')
-    const csList = new Map(csStudent)
-    console.log(`Number of students : ${STUDENT_NUMBER}`);
-    console.log(`Number of students in CS: ${csStudent.length} List: ${csList}`);
-    console.log(`Number of students in SWE: ${sweStudent.length}`);
+  const cs = [];
+  csStudent.reduce((acc, curr) => cs.push(curr[0]), cs);
 
+  const swe = [];
+  sweStudent.reduce((acc, curr) => swe.push(curr[0]), swe);
+  console.log(`Number of students : ${STUDENT_NUMBER}`);
+  console.log(`Number of students in CS: ${csStudent.length} List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudent.length} List: ${swe.join(', ')}`);
 }
 
-
-
-countStudents('./database.csv')
+module.exports = countStudents;
