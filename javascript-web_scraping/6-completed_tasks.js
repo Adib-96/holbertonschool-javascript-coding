c2577445b3414d002eec5ce9c7021b6e02f4257c
adib <121905691+Adib-96@users.noreply.github.com>
2024-03-21 13:43:34 +0100

    Update 6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
+// a script that computes the number of tasks completed by user id
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+    return;
   }
+  const users = JSON.parse(body);
+  const usersWithCoTasks = {};
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].completed) {
+      if (!(users[i].userId in usersWithCoTasks)) {
+        usersWithCoTasks[users[i].userId] = 0;
+      }
+      usersWithCoTasks[users[i].userId] += 1;
+    }
+  }
+  console.log(usersWithCoTasks);
 });
